524ccafdfb89b2d4445b9d77ba9412541d53d4e1
“Ivan <bamby4a364@gmail.com>
2018-05-05 17:58:32 +0300

    Сценарий №2, Сбербанк;

---
+++ src/test/java/InsuranceTest2.java
@@ -0,0 +1,44 @@
+import org.junit.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.*;
+import java.util.concurrent.*;
+import org.openqa.selenium.support.ui.*;
+import static org.junit.Assert.*;
+public class InsuranceTest2 {
+    WebDriver driver;
+
+    @Before
+    public void setUp() {
+        System.setProperty("webdriver.chrome.driver", "C:\\aplana\\AutoTestFirstTry\\drivers\\chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+        driver.manage().timeouts().pageLoadTimeout(15,TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+    }
+
+    @Test
+    public void Testing() throws InterruptedException {
+        driver.navigate().to("http://www.sberbank.ru/ru/person");
+        Wait<WebDriver> wait = new WebDriverWait(driver,4,1000);
+        wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.xpath("//span[@class='region-list__arrow']"))));
+        driver.findElement(By.xpath("//span[@class='region-list__arrow']")).click();
+        wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.xpath("/html/body/div[6]/div/div[2]/div[2]/div/div[1]/div/div[1]/input"))));
+        driver.findElement(By.xpath("/html/body/div[6]/div/div[2]/div[2]/div/div[1]/div/div[1]/input")).click();
+        Inserting(By.xpath("/html/body/div[6]/div/div[2]/div[2]/div/div[1]/div/div[1]/input"),"Нижегородская область");
+
+        driver.findElement(By.xpath("//span[@class='region-search-box__option']")).click();
+        assertEquals("Нижегородская область", driver.findElement(By.xpath("//span[text()='Нижегородская область']")).getText());
+        WebElement webElement = driver.findElement(By.xpath("//div[@class='footer-info']"));
+        ((JavascriptExecutor)driver).executeScript("arguments[0].scrollIntoView(false);",webElement);
+        Thread.sleep(3000);
+    }
+    public void Inserting(By x,String str){
+        driver.findElement(x).clear();
+        driver.findElement(x).sendKeys(str);
+    }
+    @After
+    public void tearsDown(){
+        driver.quit();
+    }
+
+}
