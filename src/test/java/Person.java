95fb4cfb775bc52b39d75a2a23b6eb8b3489cbec
“Ivan <bamby4a364@gmail.com>
2018-05-07 02:19:50 +0300

    Создал класс Person который может создавать рандомного человека(фамилия,имя,отчество) из 10 вариантов;

--- src/test/java/Person.java
+++ src/test/java/Person.java
@@ -1,4 +1,77 @@
-package PACKAGE_NAME;
+import java.util.*;
 
+/**
+ * @author  Osipov Ivan, student
+ */
 public class Person {
+    private String firstName,lastName, middleName; //Имя, фамилия, отчество
+    /**
+     * @return число от 1 до 10
+     */
+    public static int randomize(){
+        Random rand = new Random();
+        return rand.nextInt(11) +1;
+    }
+
+
+    /**
+     * Конструктор с параметрами
+     */
+    public Person(String firstName,String lastName,String middleName){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.middleName = middleName;
+
+    }
+
+    /**
+     * @return экземпляр Person в зависимости от рандомного значения которое генерирует метод randomize()
+     */
+    public static Person getRandomPerson() {
+
+        switch (randomize()) {
+            case 1:
+                return  new Person("Сергей", "Петров", "Владимирович");
+            case 2:
+                return  new Person("Александр", "Смирнов", "Евгеньевич");
+            case 3:
+                return  new Person("Мария", "Лунина", "Геннадьевна");
+            case 4:
+                return  new Person("Владислав", "Афанасьев", "Викторович");
+            case 5:
+                return  new Person("Максим", "Червяков", "Олегович");
+            case 6:
+                return  new Person("Антон", "Страшнов", "Абрамович");
+            case 7:
+                return  new Person("Артем", "Куранов", "Владимирович");
+            case 8:
+                return  new Person("Михаил", "Бычинин", "Игоревич");
+            case 9:
+                return  new Person("Юлия", "Тарелкина", "Семеновна");
+            case 10:
+                return  new Person("Валентин", "Пирогов", "Андреевич");
+            default: return new Person("Мавродий", "Мышкин", "Валентинович");
+        }
+    }
+
+    /**
+     * @return Имя
+     */
+    public String getFirstName() {
+        return firstName;
+    }
+
+    /**
+     * @return Фамилия
+     */
+    public String getLastName() {
+        return lastName;
+    }
+
+    /**
+     * @return Отчество
+     */
+    public String getMiddleName() {
+        return middleName;
+    }
 }
