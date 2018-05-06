49b5c0577f13d428d2cf31106d856d2e5a2f663c
“Ivan <bamby4a364@gmail.com>
2018-05-07 02:16:36 +0300

    добавил класс BaseTest в который вынес первоначальные настройки и пара методов для работы с элементами;

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,4 +1,61 @@
-package PACKAGE_NAME;
-
+import org.junit.*;
+import java.util.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.*;
+import java.util.concurrent.*;
+import org.openqa.selenium.support.ui.*;
+import static org.junit.Assert.*;
+import org.openqa.selenium.firefox.*;
+import org.openqa.selenium.chrome.*;
+/**
+ * @author  Osipov Ivan, student
+ */
 public class BaseTest {
+    protected static WebDriver driver;
+    protected static String BaseUrl;
+    public static Properties properties =  TestProperties.getInstance().getProperties();
+    static Person person;
+
+
+    @BeforeClass
+    public static void setUp() throws Exception{
+        String s = properties.getProperty("browser");
+        if (s.equals("firefox")) {
+            System.setProperty("webdriver.gecko.driver", properties.getProperty("webdriver.gecko.driver"));
+            driver = new FirefoxDriver();
+
+        } else if (s.equals("chrome")) {
+            System.setProperty("webdriver.chrome.driver", properties.getProperty("webdriver.chrome.driver"));
+            driver = new ChromeDriver();
+        }
+
+        BaseUrl = properties.getProperty("app.url1"); //или app.url2 в случае со сбером
+        driver.get(BaseUrl);
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.manage().timeouts().pageLoadTimeout(30,TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+        person = Person.getRandomPerson();
+    }
+
+    @AfterClass
+    public static void tearDown() throws Exception{
+        driver.quit();
+    }
+
+    public static void waitVisibilityOf(WebElement webElement){
+            WebDriverWait wait = new WebDriverWait(driver,10);
+            wait.until(ExpectedConditions.visibilityOf(webElement));
+    }
+    public static void waitClickableOf(WebElement webElement){
+        WebDriverWait wait = new WebDriverWait(driver,10);
+        wait.until(ExpectedConditions.elementToBeClickable(webElement));
+    }
+
+    /**
+     * Метод, который заполняет значением str поле элемента x
+     */
+    public static void Inserting(WebElement x, String str){
+        x.clear();
+        x.sendKeys(str);
+    }
 }
