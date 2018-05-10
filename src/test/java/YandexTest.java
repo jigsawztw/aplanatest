64209b5ec111ddad2ff4ec0fe5586c9a49018057
“Ivan <bamby4a364@gmail.com>
2018-05-10 16:40:09 +0300

    Добавил тест yandex.ru;

--- src/test/java/YandexTest.java
+++ src/test/java/YandexTest.java
@@ -1,26 +1,25 @@
+import pages.*;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-
-import java.util.Arrays;
-import java.util.Collection;
+import pages.BasePage;
 import java.util.concurrent.TimeUnit;
+import utils.*;
 
-public class YandexTest {
+public class YandexTest  extends BasePage {
 
     @Before
     public void setUp() {
         System.setProperty("webdriver.chrome.driver", "drivers/chromedriver.exe");
 
-        WebDriver driver = new ChromeDriver();
+        driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-        driver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
+        driver.manage().timeouts().pageLoadTimeout(15, TimeUnit.SECONDS);
         Stash.put("driver", driver);
+
         driver.get("https://yandex.ru/");
     }
 
@@ -32,13 +31,24 @@ public class YandexTest {
 
 
     @Test
-    public void search_ipad_and_check_price() {
-        new SearchPage()
-                .searchByName(itemName)
-                .savePriceAndName(1)
-                .chooseItemByPosition(1)
-                .checkPriceAndName();
-    }
+    public void search_ipad_and_check_price() throws InterruptedException{
+        new YandexMarketPage();
+        new ElectronixTV();
+        SearchPage firstSearch = new SearchPage();
+        firstSearch.setPriceFrom("20000");
+        Thread.sleep(1000);
+        firstSearch.setChooseBrand("LG");
+        Thread.sleep(1000);
+        firstSearch.setChooseBrand("Samsung");
+        firstSearch.showFiltered();
+        firstSearch.changeCountOfResultsTo12();
+        Thread.sleep(1000);
+        new SearchResultPage().saveName(1);
+        firstSearch.search((String)Stash.get(Stash.itemName));
+        new ItemPage().checkPos();
+        Thread.sleep(2000);
+
+
 
 }
 }
