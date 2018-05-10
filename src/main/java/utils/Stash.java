4e6c52bfb681de9440d8a29d06a975a0e5855f25
“Ivan <bamby4a364@gmail.com>
2018-05-10 16:39:36 +0300

    Добавил utils;

--- src/main/java/utils/Stash.java
+++ src/main/java/utils/Stash.java
@@ -1,4 +1,32 @@
 package utils;
+import org.openqa.selenium.WebDriver;
+import java.util.HashMap;
+import java.util.Map;
+import org.openqa.selenium.By;
+import org.openqa.selenium.*;
+import org.openqa.selenium.support.ui.*;
 
 public class Stash {
+    private static Map<String, Object> stash = new HashMap<String, Object>();
+    public static final String itemName = "itemName";
+
+
+    public static Object get (String key){
+        return stash.get(key);
+    }
+
+    public static void put(String key, Object value){
+        stash.put(key, value);
+    }
+
+    public static WebDriver getDriver(){
+        return (WebDriver) stash.get("driver");
+    }
+
+    public static void waitVisibilityOf(WebElement webElement){
+        WebDriverWait wait = new WebDriverWait(getDriver(),10);
+        wait.until(ExpectedConditions.visibilityOf(webElement));
+    }
+
+
 }
